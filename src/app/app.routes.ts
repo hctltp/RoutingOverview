6724754da4948acaf59ce34102c98fabaeba4aa8
hamdicataltepe <hamdicataltepe@gmail.com>
2024-02-08 20:33:23 +0300

    angular router overview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        title:'App Home Page',
+        component: HomeComponent,
+    },
+    {
+        path: 'user',
+        title:'User Page',
+        component: UserComponent,
+    },
+];
